acee621534dad8fbb7594a007624f4e3a465be98
Sameer Bhat <sameerbhat76@gmail.com>
2020-05-07 13:38:11 +0530

    change details

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -98,6 +98,13 @@ ClassicEditor.defaultConfig = {
 			'imageTextAlternative'
 		]
 	},
+	// table: {
+	// 	contentToolbar: [
+	// 		'tableColumn',
+	// 		'tableRow',
+	// 		'mergeTableCells'
+	// 	]
+	// },
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
